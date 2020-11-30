f4480608438c115e692932eee39fe1931c252153
theDreamer911 <thetrulydreamer@gmail.com>
2020-11-30 21:54:31 +0700

    'first'

---
+++ II/app.py
@@ -0,0 +1,97 @@
+# course = "Python Programming"
+# print(len(course))
+
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])
+# print(course[5:])
+# print(course[:3])
+# print(course[5:8])
+
+
+# course = "Python \n'geet"
+
+# print(course)
+
+# first = "Hand"
+# last = "Pratama"
+# # full = first + " " + last
+# # full = f"{first} {last}"
+# full = f"{len(first)} {3+7}"
+
+
+# print(full)
+
+
+# String Method
+
+# course = "     program pythonnista  "
+# upper = course.upper()
+# lower = course.lower()
+# title = course.title()
+# strip = course.strip()
+# lstrip = course.lstrip()
+# rstrip = course.rstrip()
+# print(upper)
+# print(lower)
+# print(title)
+# print(strip)
+# print(lstrip)
+# print(rstrip)
+
+# print(course.find("pro"))
+# print(course.replace("pro", "BAM"))
+
+# print('pro' in course)
+# print('pro' not in course)
+
+# j = 3
+# a = 1
+# a = 1.1
+# a = 1 + 2j
+# print(a)
+
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+
+# x = 10
+# x = x + 3
+# x += 3
+
+# import math
+
+# # print(round(2.9))
+# # print(abs(-2.9))
+# # print(round(abs(-2.9)))
+
+# print(math.ceil(2.8))
+# print(math.copysign(-2, 1))
+# print(math.fabs(-2.1))
+# print(math.factorial(5))
+# print(math.floor(5.9))
+
+# print(math.ceil(math.floor(-5.9)))
+
+
+# # x = int(input("x: "))
+# x = input("x: ")
+# print(type(x))
+# y = int(x) + 1
+# # print(y)
+# # print(type(y))
+# print(f"x: {x}, y: {y}")
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+
+fruit = "sedap"
+print(fruit[1:-1])
